2c64cdd864bf6b660defcb37a9d3e3f9340b43ac
no <leachmanh@gmail.com>
2022-10-05 00:28:18 -0500

    some initial test routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateStudentDto } from './create-student.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post()
+  async create(@Body() createStudentDto: CreateStudentDto) {
+    return 'this action adds a Student';
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<any[]> {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} resource`;
   }
 }
